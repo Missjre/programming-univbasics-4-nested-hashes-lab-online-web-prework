acf5a0809969332dc52a6cfa4546aa2742b5f349
Jalysa Reason <jalysareason@gmail.com>
2019-08-29 23:37:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,13 +71,10 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  programmer_hash =
-  
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
 
 	programmer_hash = 
  		{
@@ -106,7 +103,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
