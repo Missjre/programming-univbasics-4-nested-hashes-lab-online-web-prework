a940dc0c9a73c6d66a86a1f0ff1d0981ee7c4b63
Jalysa Reason <jalysareason@gmail.com>
2019-08-29 23:12:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,13 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	{
- 
+  programmer_hash =
+	
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        }
+      }
 	
 	programmer_hash = 
  		{
